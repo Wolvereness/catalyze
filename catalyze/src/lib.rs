a9fa9f463f1add6f4b0aca6208dfc013b1bdc885
Wesley Wolfe <wesley.d.wolfe+git@gmail.com>
2024-01-06 15:52:24 -0600

    Add allocation strategy

--- catalyze/src/lib.rs
+++ catalyze/src/lib.rs
@@ -41,6 +41,7 @@ pub mod method;
 pub mod oneof;
 pub mod package;
 pub mod service;
+mod allocation;
 
 pub(crate) type HashMap<K, V> = ahash::HashMap<K, V>;
 pub(crate) type HashSet<V> = ahash::HashSet<V>;
